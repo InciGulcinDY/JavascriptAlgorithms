8acca4fd2b5fdfbb6fd801648e833b9d7b0f1753
İnci Gülçin Durak Yolcu <113159986+InciGulcinDY@users.noreply.github.com>
2024-10-22 18:33:11 +0300

    Init

---
+++ dataTypes.js
@@ -0,0 +1,26 @@
+function performOperation(secondInteger, secondDecimal, secondString) {
+    // Declare a variable named 'firstInteger' and initialize with integer value 4.
+    const firstInteger = 4;
+    
+    // Declare a variable named 'firstDecimal' and initialize with floating-point value 4.0.
+    const firstDecimal = 4.0;
+    
+    // Declare a variable named 'firstString' and initialize with the string "HackerRank".
+    const firstString = 'HackerRank ';
+    
+    // Write code that uses console.log to print the sum of the 'firstInteger' and 'secondInteger' (converted to a Number        type) on a new line.
+    console.log(firstInteger + parseInt(secondInteger, 10));
+    
+    // Write code that uses console.log to print the sum of 'firstDecimal' and 'secondDecimal' (converted to a Number            type) on a new line.
+    console.log(firstDecimal + Number(secondDecimal));
+    
+    // Write code that uses console.log to print the concatenation of 'firstString' and 'secondString' on a new line. The        variable 'firstString' must be printed first.
+    console.log(firstString + secondString);
+}
+
+
+function convertDataToInteger() {
+    const data = document.getElementById("dataToBeInteger").value; // Use .value to get input data
+    const result = parseInt(data); // Convert the string to integer
+    document.getElementById("resul1").innerHTML = isNaN(result) ? 'Invalid input' : result; // Use .innerHTML to update the result
+}
